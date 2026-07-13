ac253a4c995b90b155217e2147773978b59007a6
Srujalaudarya <srujalaudarya249@gmail.com>
2026-07-13 11:32:44 +0530

    update location delte page

--- master_activities/locations/locations_delete.php
+++ master_activities/locations/locations_delete.php
@@ -2,11 +2,11 @@
 global $conn;
 include("../../includes/auth.php");
 include("../../config/db.php");
-include("../../includes/header.php");
-include("../../includes/sidebar.php");
 
-$id = $_GET['id'];
+$id = mysqli_real_escape_string($conn, $_GET['id']);
 
 mysqli_query($conn, "DELETE FROM locations WHERE location_id=$id");
 
-// header("Location: locations_list.php");
+header("Location: " . ROUTE_LOCATIONS);
+exit();
+?>
